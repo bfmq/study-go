e280061dc8ed47d735542ed10ab26947298b3038
lipengfei <617351843@qq.com>
2021-10-15 17:28:01 +0800

    287

---
+++ leetcode/387first-unique-character-in-a-string/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func firstUniqChar(s string) int {
+
+}
+
+func main() {
+	result := firstUniqChar("leetcode")
+	fmt.Println(result)
+	result = firstUniqChar("loveleetcode")
+	fmt.Println(result)
+}
