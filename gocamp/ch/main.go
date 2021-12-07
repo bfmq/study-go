5cc49673c92be2810237b131aecafda9fced9610
lipf <lipf2@sudoprivacy.com>
2021-12-07 17:35:11 +0800

    信道关闭测试

--- gocamp/ch/main.go
+++ gocamp/ch/main.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func f1() {
-	fmt.Println("i'm f1")
+func f1(ch chan int, x, y, z int) {
+	ch <- x + y + z
 }
 
 func main() {
-	ch := make(chan bool, 3)
-	fmt.Println(ch)
-	go f1()
+	result := make([]int, 0, 2)
+	ch := make(chan int, 3)
+	go f1(ch, 1, 2, 3)
+	go f1(ch, 4, 5, 6)
+
+	for {
+		select {
+		case r := <-ch:
+			result = append(result, r)
+			if len(result) == 2 {
+				close(ch)
+				return
+			}
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("==============")
+		}
+	}
 }
