35f3292f068358739f4bd3a97f3e24c9bf12fdeb
lipf <lipf2@sudoprivacy.com>
2021-12-07 19:17:22 +0800

    作业11要求1完成

--- cncamp/day11/homework/main.go
+++ cncamp/day11/homework/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -12,7 +14,6 @@ import (
 func IPCodeMiddle() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
-		// 要求3：获取客户端地址、本次返回状态码，并打印日志到控制台
 		ip := c.Request.RemoteAddr
 		code := c.Writer.Status()
 		log.Println("客户端地址---------------->", ip)
@@ -26,30 +27,26 @@ func main() {
 	router.Use(IPCodeMiddle())
 
 	{
-		// 要求4：提供healthz接口
 		router.GET("/healthz/", healthzGetHandle)
+		router.GET("/delay/", delayGetHandle)
 	}
 
 	router.Run(":8000")
 }
 
 func healthzGetHandle(c *gin.Context) {
-	// 要求1：将request header封回response header
 	for k, v := range c.Request.Header {
 		c.Header(k, v[0])
 	}
 
-	// 要求2：获取当前系统env相关环境信息封回response header
 	version := getVersion()
 	c.Header("VERSION", version)
 
 	c.JSON(http.StatusOK, gin.H{
-		// 要求4：healthz接口返200码
 		"code": http.StatusOK,
 	})
 }
 
-// 写代码时没有VERSION/Version/version这几个环境变量，可能系统变量名不太一样吧
 func getVersion() string {
 	version := os.Getenv("VERSION")
 	if version == "" {
@@ -57,4 +54,15 @@ func getVersion() string {
 		version = os.Getenv("VERSION")
 	}
 	return version
+}
+
+// 要求1：随机等待1-2000ms返回
+func delayGetHandle(c *gin.Context) {
+	rand.Seed(time.Now().UnixNano())
+	sleepTime := rand.Intn(2000)
+	time.Sleep(time.Millisecond * time.Duration(sleepTime))
+	c.JSON(http.StatusOK, gin.H{
+		"code":      http.StatusOK,
+		"sleepTime": sleepTime,
+	})
 }
