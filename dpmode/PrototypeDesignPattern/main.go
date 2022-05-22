02bda77a18548d7b50195ac9ecb0242501d2c46a
617351843@qq.com <617351843@qq.com>
2022-05-23 00:05:16 +0800

    原型模式

---
+++ dpmode/PrototypeDesignPattern/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Prototype struct {
+	Elem    string
+	Content string
+}
+
+func (p *Prototype) Copy() *Prototype {
+	return &(*p)
+}
+
+func main() {
+	ptObj := new(Prototype)
+	ptObj.Elem = "this is prototype"
+	ptCopy := ptObj.Copy()
+	ptCopy.Content = "this is prototype clone "
+
+	fmt.Println(ptObj)
+	fmt.Println(ptCopy)
+
+}
