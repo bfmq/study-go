71910cd0931d6c3a2aad4719f3a6a6b628f2ddb7
lipf <lipf2@sudoprivacy.com>
2022-09-13 17:27:55 +0800

    guess-numbers

--- leetcode/second/guess-numbers/main.go
+++ leetcode/second/guess-numbers/main.go
@@ -1,9 +1,13 @@
+// https://leetcode.cn/problems/guess-numbers/
 package main
 
 import "fmt"
 
 func game(guess []int, answer []int) int {
-	// 3次直接循环对比即可
+	// guess跟answer是2人已经各自的答案顺序
+	// 同下标为同一次游戏
+	// 因此直接比较值是否相等即可知道本次游戏是否猜对
+	// 最终将猜对累加即可
 	var res int
 	for i := 0; i < 3; i++ {
 		if guess[i] == answer[i] {
