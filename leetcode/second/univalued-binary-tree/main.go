636386be1a491acb96c790d6365681e69f7d3053
lipf <lipf2@sudoprivacy.com>
2022-09-16 19:14:55 +0800

    univalued-binary-tree

---
+++ leetcode/second/univalued-binary-tree/main.go
@@ -0,0 +1,17 @@
+// https://leetcode.cn/problems/univalued-binary-tree/
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isUnivalTree(root *TreeNode) bool {
+	return root == nil || (root.Left == nil || root.Val == root.Left.Val && isUnivalTree(root.Left)) &&
+		(root.Right == nil || root.Val == root.Right.Val && isUnivalTree(root.Right))
+
+}
+
+func main() {
+}
