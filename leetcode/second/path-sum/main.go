4a4d420c2954167a6fe942034dd6d15270f64373
lipf <lipf2@sudoprivacy.com>
2022-09-19 18:37:36 +0800

    path-sum

--- leetcode/second/path-sum/main.go
+++ leetcode/second/path-sum/main.go
@@ -1,3 +1,4 @@
+// https://leetcode.cn/problems/path-sum/
 package main
 
 type TreeNode struct {
@@ -10,9 +11,11 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
 	if root == nil {
 		return false
 	}
+	// 结束了比较最终值
 	if root.Left == nil && root.Right == nil {
 		return targetSum == root.Val
 	}
+	// 左右有一路径满足即可
 	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
 }
 
